cbd8e6022c7f681c0baf6194bc239a43fd94714a
Jack Lingle <captainjack0818@gmail.com>
2023-07-24 14:02:36 -0400

    add mutation for form

---
+++ phonebook-frontend/src/components/PersonForm.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_PERSON, ALL_PERSONS } from "../queries";
+
+function PersonForm() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+
+  const [createPerson] = useMutation(CREATE_PERSON, {
+    refetchQueries: [{ query: ALL_PERSONS }],
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createPerson({ variables: { name, phone, street, city } });
+    setName("");
+    setPhone("");
+    setStreet("");
+    setCity("");
+  };
+
+  return (
+    <div>
+      <h2>Create New</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label>
+          <input
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="city">City</label>
+          <input
+            id="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="street">Street</label>
+          <input
+            id="street"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default PersonForm;
