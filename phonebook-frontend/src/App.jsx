ef4ea772227eddef9ffe9042f127bb7b1da50a17
Jack Lingle <captainjack0818@gmail.com>
2023-07-24 13:40:06 -0400

    add persons component for expanding to show addres

--- phonebook-frontend/src/App.jsx
+++ phonebook-frontend/src/App.jsx
@@ -1,3 +1,27 @@
-function App() {}
+import { gql, useQuery } from "@apollo/client";
+import Persons from "./components/persons";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+function App() {
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Persons persons={result.data.allPersons} />
+    </div>
+  );
+}
 
 export default App;
