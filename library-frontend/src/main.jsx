9f53576bba889582253e38f5bdfcb53adc14aa60
Jack Lingle <captainjack0818@gmail.com>
2023-07-25 16:27:50 -0400

    add basic initialization

---
+++ library-frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
+const client = new ApolloClient({
+  uri: "http://localhost:400)",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+);
